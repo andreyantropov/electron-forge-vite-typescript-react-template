946a696effd307b3784eb2d1bbca44ad0d70dc34
Андрей Антропов <andreyantropov.1994@gmail.com>
2024-12-24 11:32:44 +0500

    fix: исправлена сборка проекта.

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,11 @@
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig(({ mode }) => ({
+  build: {
+    sourcemap: mode === "development",
+    rollupOptions: {
+      external: mode === "production" ? [] : [/node_modules/],
+    },
+  },
+}));
